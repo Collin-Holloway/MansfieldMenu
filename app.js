906c40d5da1561c8fa3197a6938eb84a52c94c5b
Collin Holloway <collinjholloway@gmail.com>
2022-09-29 17:50:05 -0500

    added images folder

---
+++ app.js
@@ -0,0 +1,77 @@
+//itmes
+const menu = [
+    {
+      id: 1,
+      title: "buttermilk pancakes",
+      category: "breakfast",
+      price: 15.99,
+      img: "./images/item-1.jpeg",
+      desc: `A delicous stack of buttermilk pancakes with syrup, powerder sugar, and fruit toppings. `,
+    },
+    {
+      id: 2,
+      title: "diner double",
+      category: "lunch",
+      price: 13.99,
+      img: "./images/item-2.jpeg",
+      desc: `A bacon cheeseburger served with steak fries. `,
+    },
+    {
+      id: 3,
+      title: "godzilla milkshake",
+      category: "shakes",
+      price: 6.99,
+      img: "./images/item-3.jpeg",
+      desc: `An ice colde milkshake topped with a sprinkled donut.`,
+    },
+    {
+      id: 4,
+      title: "country delight",
+      category: "breakfast",
+      price: 20.99,
+      img: "./images/item-4.jpeg",
+      desc: `Eggs served sunny side up over toast with a side of baked beans and cheese.`,
+    },
+    {
+      id: 5,
+      title: "egg attack",
+      category: "lunch",
+      price: 22.99,
+      img: "./images/item-5.jpeg",
+      desc: `A hamburger with lettuce, tomatoes and eggs. `,
+    },
+    {
+      id: 6,
+      title: "oreo dream",
+      category: "shakes",
+      price: 18.99,
+      img: "./images/item-6.jpeg",
+      desc: `An orea cookie milkshake`,
+    },
+    {
+      id: 7,
+      title: "bacon overflow",
+      category: "breakfast",
+      price: 8.99,
+      img: "./images/item-7.jpeg",
+      desc: `A bacon egg'n cheese biscuit. `,
+    },
+    {
+      id: 8,
+      title: "american classic",
+      category: "lunch",
+      price: 12.99,
+      img: "./images/item-8.jpeg",
+      desc: `A classic hamburger with a side of french fries. `,
+    },
+    {
+      id: 9,
+      title: "quarantine buddy",
+      category: "shakes",
+      price: 16.99,
+      img: "./images/item-9.jpeg",
+      desc: `Sharing a milkshake with a friend.`,
+    },
+  ];
+
+  
